5c3d9c6ce5533f7eb8933e08ebbd6f40b7c84b18
Michal <michal@MacBook-Air-Tomcik.local>
2026-03-03 21:35:13 +0100

    Redirect fudly.sk to www

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
-//next.config.mjs
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "fudly.sk",
+          },
+        ],
+        destination: "https://www.fudly.sk/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
